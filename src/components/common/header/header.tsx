66b831e586131d67788316963f9df430cef989b2
Nazariy <nazar260804@gmail.com>
2024-02-08 01:33:58 +0200

    * header refactor

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -2,23 +2,22 @@ import { FC, useState } from "react";
 import HeaderMenu from "./components/header-menu/header-menu";
 
 import styles from "./styles.module.scss";
-import Image from "../image/image";
 
 const Header: FC = () => {
     const [isHeaderMenuShown, setIsHeaderMenuShown] = useState<boolean>(false);
     return (
-        <div className={styles.header}>
+        <header>
             <h1 className={styles.header__textLogo}>
-                <span className={styles.header_textLogo_coloredFragment}>
+                <span className={styles.coloredFragment}>
                     PURPLE
                 </span>{" "}
                 MARKET
             </h1>
             <div className={styles.header__userData}>
-                <p className={styles.header__username}>DoopSnogg</p>
-                <Image
-                    src={"/profile_template.jpg"}
-                    preset={"avatar"}
+                <img
+                    src="/profile_template.jpg"
+                    alt="avatar"
+                    className={styles.header__avatar}
                     onClick={() =>
                         setIsHeaderMenuShown((prevState) => !prevState)
                     }
@@ -27,7 +26,7 @@ const Header: FC = () => {
                     {isHeaderMenuShown && <HeaderMenu />}
                 </div>
             </div>
-        </div>
+        </header>
     );
 };
 
