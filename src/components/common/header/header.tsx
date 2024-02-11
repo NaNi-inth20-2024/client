0b8b51d3d7b82610c9ca38bb7221f20e5323a5ee
Nazariy <nazar260804@gmail.com>
2024-02-11 02:19:07 +0200

    + header menu

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -1,17 +1,27 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import HeaderMenu from "./components/header-menu/header-menu";
 
 import styles from "./styles.module.scss";
 
 const Header: FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
         <header>
             <h1 className={styles.header__textLogo}>
                 <span className={styles.coloredFragment}>PURPLE</span> MARKET
             </h1>
             <div className={styles.header__userData}>
+                <svg
+                    className={styles.bars}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 448 512"
+                    onClick={() => setIsMenuOpen(!isMenuOpen)}
+                >
+                    <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+                </svg>
                 <div className={styles.header__menuContainer}>
-                    <HeaderMenu />
+                    <HeaderMenu visible={isMenuOpen} username="Nigga" />
                 </div>
             </div>
         </header>
