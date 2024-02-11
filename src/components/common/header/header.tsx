17258fed075ed4795b231482cc0a35f0e6a13fdf
Nazariy <nazar260804@gmail.com>
2024-02-11 21:49:51 +0200

    + log in button

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -5,6 +5,8 @@ import { useRevalidateQuery, authApi } from "@/store/auth.api";
 
 import styles from "./styles.module.scss";
 import { useAppDispatch } from "@/store/hooks";
+import Button from "../button/button";
+import { Link } from "react-router-dom";
 
 const Header: FC = () => {
     const dispatch = useAppDispatch();
@@ -24,27 +26,35 @@ const Header: FC = () => {
     return (
         <header>
             <h1 className={styles.header__textLogo}>
-                <span className={styles.coloredFragment}>PURPLE</span> MARKET
+                <Link to="/">
+                    <span className={styles.coloredFragment}>PURPLE</span>{" "}
+                    MARKET
+                </Link>
             </h1>
-            <div className={styles.header__userData}>
-                {userData?.username && (
-                    <svg
-                        className={styles.bars}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 448 512"
-                        onClick={() => setIsMenuOpen(!isMenuOpen)}
-                    >
-                        <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-                    </svg>
-                )}
-                <div className={styles.header__menuContainer}>
-                    <HeaderMenu
-                        visible={isMenuOpen}
-                        username={userData?.username}
-                        onLogout={handleLogout}
-                    />
+            {userData?.username ? (
+                <div className={styles.header__userData}>
+                    <>
+                        <svg
+                            className={styles.bars}
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 448 512"
+                            onClick={() => setIsMenuOpen(!isMenuOpen)}
+                        >
+                            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+                        </svg>
+
+                        <div className={styles.header__menuContainer}>
+                            <HeaderMenu
+                                visible={isMenuOpen}
+                                username={userData?.username}
+                                onLogout={handleLogout}
+                            />
+                        </div>
+                    </>
                 </div>
-            </div>
+            ) : (
+                <Button name="Sign in" link="/auth" />
+            )}
         </header>
     );
 };
